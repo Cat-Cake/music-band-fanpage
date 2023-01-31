c6ab03aac4f8200750b0ad88a0c40d5dc1dea935
CatCake <lucas.manot@gmail.com>
2023-01-31 09:59:48 +0100

    adding space

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,24 +1,31 @@
-var videos = [ "./assets/videos/METALKINGDOM.mp4", "./assets/videos/BABYMETAL–DivineAttack.mp4", "./assets/videos/BABYMETAL-Monochrome.mp4"];
+let videos = [ "./assets/videos/METALKINGDOM.mp4", "./assets/videos/BABYMETAL–DivineAttack.mp4", "./assets/videos/BABYMETAL-Monochrome.mp4"];
+let titles = ["METALKINGDOM", "DivineAttack", "Monochrome"]
 
-var currentIndex = 0;
-var currentVideo = document.querySelector("#current-video");
-var prevBtn = document.querySelector("#prev-btn");
-var nextBtn = document.querySelector("#next-btn");
+let currentIndex = 0;
+let currentVideo = document.querySelector("#current-video");
+let prevBtn = document.querySelector("#prev-btn");
+let nextBtn = document.querySelector("#next-btn");
+
+let titlesContainer = document.querySelector('.titles-container')
 
 prevBtn.addEventListener("click", function() {
-  currentIndex--;
-  if (currentIndex < 0) {
-    currentIndex = videos.length - 1;
-  }
-  currentVideo.src = videos[currentIndex];
-  currentVideo.play();
+currentIndex--;
+if (currentIndex < 0) {
+currentIndex = videos.length - 1;
+}
+currentVideo.src = videos[currentIndex];
+let currentTitles = titles[currentIndex];
+currentVideo.play();
+titlesContainer.innerHTML = currentTitles;
 });
 
 nextBtn.addEventListener("click", function() {
-  currentIndex++;
-  if (currentIndex >= videos.length) {
-    currentIndex = 0;
-  }
-  currentVideo.src = videos[currentIndex];
-  currentVideo.play();
+currentIndex++;
+if (currentIndex >= videos.length) {
+currentIndex = 0;
+}
+currentVideo.src = videos[currentIndex];
+let currentTitles = titles[currentIndex];
+currentVideo.play();
+titlesContainer.innerHTML = currentTitles;
 });
